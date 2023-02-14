8edb5c672ed8bf4a97307cdb7b38c026a228bb87
alex <sasaluginin123@gmail.com>
2023-02-14 20:42:27 +0400

    second homework

--- 1.php
+++ 1.php
@@ -1,16 +1,20 @@
 <?php
+// 1
 function square($num) {
     return $num**2;
 }
 
+// 2
 function sum($num1, $num2) {
     return $num1 + $num2;
 }
 
+// 3
 function idk($num1, $num2, $num3) {
     return ($num1-$num2)/$num3;
 }
 
+// 4
 function day($num) {
     $days = [1=>"Понедельник", 2=>"Вторник", 3=>"Среда", 4=>"Четверг", 5=>"Пятница", 6=>"Суббота", 7=>"Воскресенье"];
     return $days[$num];
---
+++ 2.php
@@ -0,0 +1,11 @@
+<?php
+// 1
+function wtf($a, $b) {
+    // return "Предложение" ?????
+    return $a." ".$b;
+}
+
+// 2
+$arr = [2, 1, 6, 9, 2, 8];
+sort($arr);
+
