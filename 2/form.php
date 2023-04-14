c1af7843d37939c1b1fa618d41e0177fbf55793f
alex <sasaluginin123@gmail.com>
2023-04-14 17:00:22 +0400

    update

---
+++ 2/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="message.php" method="post">
+        <input name="name" type="text" placeholder="Имя">
+        <input name="age" type="text" placeholder="Возраст">
+        <button>Отправить</button>
+    </form>
+</body>
+</html>
